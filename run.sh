3593682e9b42401706cb68440f035aa4dd67817e
Niels Højen <niels@hojen.net>
2018-04-28 08:27:52 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [ $FWSERVER_BACKUP -eq 1 ]; then
+  /backup_server_osx_linux.sh setup daily /backup
+fi
+
 /etc/init.d/fw-server start
 /usr/bin/tail -F /private/var/log/fwxserver.log
