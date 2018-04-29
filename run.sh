d96530783e75bc036d0d966a4a35181ef2388a50
Niels Højen <niels@hojen.net>
2018-04-29 11:09:50 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+yum install -y --nogpgcheck fwxserver-$FWSERVER_VERSION-1.0.x86_64.rpm
+
 if [ $FWSERVER_BACKUP -eq 1 ]; then
   /backup_server_osx_linux.sh setup daily /backup
 fi
 
-/etc/init.d/fw-server start
+if [[ ! $(/etc/init.d/fw-server status | /usr/bin/grep running) ]]; then
+fi
+
+#/etc/init.d/fw-server start
 /usr/bin/tail -F /private/var/log/fwxserver.log
