51554c1ff9af79434f3ba51855cfa5a2b752f137
Niels Højen <niels@hojen.net>
2018-04-29 12:58:54 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-yum install -y --nogpgcheck fwxserver-$FWSERVER_VERSION-1.0.x86_64.rpm
+if [[ ! $(/bin/ls -A /usr/local/filewave) ]]; then
+  yum reinstall -y --nogpgcheck fwxserver-$FWSERVER_VERSION-1.0.x86_64.rpm
+fi
 
 if [ $FWSERVER_BACKUP -eq 1 ]; then
   /backup_server_osx_linux.sh setup daily /backup
