cf6946896ff6ef1f26ef49f145313c9fb88b7991
Niels Højen <niels@hojen.net>
2018-04-29 13:01:53 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,12 +5,15 @@ if [[ ! $(/bin/ls -A /usr/local/filewave) ]] || [[ ! $(/bin/ls -A /fwxserver) ]]
   yum reinstall -y --nogpgcheck fwxserver-$FWSERVER_VERSION-1.0.x86_64.rpm
 fi
 
+# Setup the backup
 if [ $FWSERVER_BACKUP -eq 1 ]; then
   /backup_server_osx_linux.sh setup daily /backup
 fi
 
+# Make sure we're up
 if [[ ! $(/etc/init.d/fw-server status | /usr/bin/grep running) ]]; then
   /etc/init.d/fw-server start
 fi
 
+# Tail out the log for convenience
 /usr/bin/tail -F /private/var/log/fwxserver.log
